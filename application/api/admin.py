3005ff1a50721cf653c7bd2123918de597e0a381
asritha548 <asritha548@gmail.com>
2021-07-01 12:35:40 -0700

    deleting them to resolve the code conflicts

--- application/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import auto_scraper,url
-
-admin.site.register(auto_scraper)
-admin.site.register(url)
--- website-analysis/auto_website_scraper/items.py
+++
@@ -1,6 +0,0 @@
-from scrapy.item import Item, Field
-
-class AutoScraperItem(Item):
-    # define the fields for your item here like:
-    telefon = Field()
-    url_id= Field()
